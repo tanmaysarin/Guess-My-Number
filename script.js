2c6a2d3e591e4a60996af3c71369a03bd5b42c58
tanmaysarin <tanmaysarin@gmail.com>
2022-06-26 17:31:28 -0700

    adding first event listener

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = '13';
+document.querySelector('.score').textContent = '5';
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '📛 No Number!';
+  }
+});
