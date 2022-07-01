aabd1658316837b4707bdcd2fcc3a2a292977218
tanmaysarin <tanmaysarin@gmail.com>
2022-07-01 00:27:47 -0700

    working game code

--- script.js
+++ script.js
@@ -11,11 +11,78 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+let highscore = 0;
+
+// Implementing 'check' button functionality
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
 
+  // when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '📛 No Number!';
+
+    // when player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // when the guess is high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High!';
+      score = score - 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // when guess is low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too Low!';
+      score = score - 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
+});
+
+// implementing 'again button' functionality
+document.querySelector('.again').addEventListener('click', function () {
+  // reseting the score
+  score = 20;
+  document.querySelector('.score').textContent = score;
+
+  // reseting the hidden number
+  document.querySelector('.number').textContent = '?';
+
+  // reseting the message
+  document.querySelector('.message').textContent = 'Start guessing...';
+
+  // reseting the body color
+  document.querySelector('body').style.backgroundColor = '#222';
+
+  // reseting the number width
+  document.querySelector('.number').style.width = '15rem';
+
+  // setting the value entered by the user to be null
+  document.querySelector('.guess').value = null;
+
+  // getting a new secret number
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
 });
